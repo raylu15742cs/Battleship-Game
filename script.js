f0eba3437c777c3612ee33346a9999024b1a6595
raylu15742cs <raylu15742cs@gmail.com>
2022-07-26 11:55:58 -0700

    create gameboard

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // create 7 ships [5,4,3,2,2]
+const gamecontainer = document.getElementById('gameboard');
+
 let shipstorage = [
   { title: 'Carrier', length: '5' },
   { title: 'Battleship', length: '4' },
@@ -6,22 +8,38 @@ let shipstorage = [
   { title: 'Destroyer', length: '2' },
   { title: 'Destroyertwo', length: '2' },
   { title: 'Submarine', length: '1' },
-  { title: 'Submarinetwo', length: '1' }
+  { title: 'Submarinetwo', length: '1' },
 ];
 function ship(shipstorage) {
-  let answer = []
-  for(let i = 0 ; i < shipstorage.length ; i++) {
-    let current = {}
-    for(let j = 0; j < shipstorage[i].length; j++) {
+  let answer = [];
+  for (let i = 0; i < shipstorage.length; i++) {
+    let current = {};
+    for (let j = 0; j < shipstorage[i].length; j++) {
       //create each individual ship
-      current[j] = `${shipstorage[i].title}${j}`
+      current[j] = `${shipstorage[i].title}${j}`;
     }
-    answer[i] = current
+    answer[i] = current;
   }
-  return Object.keys(answer[0]).length
+  return Object.keys(answer[0]).length;
 }
 
-function hit(ship) {
+function hit() {}
 
+function sink() {}
+
+function gameboard(height) {
+  for (let i = 0; i < height; i++) {
+    bigbox = document.createElement('div');
+    gamecontainer.appendChild(bigbox);
+    for (let j = 0; j < height; j++) {
+      let box = document.createElement('div');
+      box.classList.add(`${i}${j}`);
+      box.classList.add("box")
+      box.style.height = '100px';
+      box.style.width = '100px';
+      bigbox.appendChild(box);
+    }
+  }
 }
-module.exports = ship;
+
+gameboard(10);
--- script.test.js
+++ script.test.js
@@ -1,13 +1,15 @@
-const sum = require('./script');
+const sum = require('./script').default;
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum([
-  { title: 'Carrier', length: '5' },
-  { title: 'Battleship', length: '4' },
-  { title: 'Cruiser', length: '3' },
-  { title: 'Destroyer', length: '2' },
-  { title: 'Destroyertwo', length: '2' },
-  { title: 'Submarine', length: '1' },
-  { title: 'Submarinetwo', length: '1' }
-])).toBe("Carrier");
+  expect(
+    sum([
+      { title: 'Carrier', length: '5' },
+      { title: 'Battleship', length: '4' },
+      { title: 'Cruiser', length: '3' },
+      { title: 'Destroyer', length: '2' },
+      { title: 'Destroyertwo', length: '2' },
+      { title: 'Submarine', length: '1' },
+      { title: 'Submarinetwo', length: '1' },
+    ])
+  ).toBe('Carrier');
 });
