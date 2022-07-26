4cd6b4baad54a40cc7c8945cff9a7aa8f4c40766
Raymond Lu <raylucs15742@gmail.com>
2022-07-26 09:16:47 -0700

    add shipstorage object

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function ship(a, b) {
-  return a + b;
+// create 7 ships [5,4,3,2,2]
+let shipstorage = [
+  { title: 'Carrier', length: '5' },
+  { title: 'Battleship', length: '4' },
+  { title: 'Cruiser', length: '3' },
+  { title: 'Destroyer', length: '2' },
+  { title: 'Destroyer2', length: '2' },
+  { title: 'Submarine', length: '1' },
+  { title: 'Submarine2', length: '1' }
+];
+function ship(shipstorage) {
+  for(let i = 0 ; i < shipstorage.length ; i++) {
+    
+  }
 }
-module.exports = sum;
+module.exports = ship;
--- script.test.js
+++ script.test.js
@@ -1,5 +1,13 @@
 const sum = require('./script');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum([
+  { title: 'Carrier', length: '5' },
+  { title: 'Battleship', length: '4' },
+  { title: 'Cruiser', length: '3' },
+  { title: 'Destroyer', length: '2' },
+  { title: 'Destroyer2', length: '2' },
+  { title: 'Submarine', length: '1' },
+  { title: 'Submarine2', length: '1' }
+])).toBe("Carrier");
 });
