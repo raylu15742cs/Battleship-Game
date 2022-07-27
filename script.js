62044f4cfe95551c5b7ce1efc2aa2c0a398a7ed4
Raymond Lu <raylucs15742@gmail.com>
2022-07-27 14:38:00 -0700

    add win condition

--- script.js
+++ script.js
@@ -2,52 +2,64 @@
 const gamecontainer = document.getElementById('gameboard');
 
 let shipstorage = [
-  { title: 'Carrier', length: '5', count: 0, max: 59 },
-  { title: 'Battleship', length: '4', count: 0, max: 69 },
-  { title: 'Cruiser', length: '3', count: 0, max: 79 },
-  { title: 'Destroyer', length: '2', count: 0, max: 89 },
-  { title: 'Submarine', length: '1', count: 0, max: 99 },
+  { title: 'Carrier', length: '5', count: 0, max: 59, sunk: false },
+  { title: 'Battleship', length: '4', count: 0, max: 69, sunk: false },
+  { title: 'Cruiser', length: '3', count: 0, max: 79, sunk: false },
+  { title: 'Destroyer', length: '2', count: 0, max: 89, sunk: false },
+  { title: 'Submarine', length: '1', count: 0, max: 99, sunk: false },
 ];
 
 let computershipstorage = [
-  { title: 'CompCarrier', length: '5', count: 0, max: 59 },
-  { title: 'CompBattleship', length: '4', count: 0, max: 69 },
-  { title: 'CompCruiser', length: '3', count: 0, max: 79 },
-  { title: 'CompDestroyer', length: '2', count: 0, max: 89 },
-  { title: 'CompSubmarine', length: '1', count: 0, max: 99 },
+  { title: 'compCarrier', length: '5', count: 0, max: 59, sunk: false },
+  { title: 'compBattleship', length: '4', count: 0, max: 69, sunk: false },
+  { title: 'compCruiser', length: '3', count: 0, max: 79, sunk: false },
+  { title: 'compDestroyer', length: '2', count: 0, max: 89, sunk: false },
+  { title: 'compSubmarine', length: '1', count: 0, max: 99, sunk: false },
 ];
 
 function hit(e) {
-  console.log(e.path)
   let targets = document.getElementById(e.path[0].id);
   targets.classList.remove('hits')
   targets.classList.remove('bluetime');
   targets.innerText = 'X';
   targets.classList.add('hit');
-  if (shipstorage[e.path[0].id[e.path[0].id[0]]] ==='s') {
+  targets.removeEventListener("click", hit)
+  if (e.path[0].id[0] !== 'c') {
     shipstorage[e.path[0].id[e.path[0].id.length - 2]].count++;
   } else {
     computershipstorage[e.path[0].id[e.path[0].id.length - 2]].count++;
   }
-  isSunk();
+  //hitback();
+  isSunk(e.path[0].id);
 }
 
-function isSunk() {
-  for(let i = 0 ; i < shipstorage.length ; i++) {
-    if(shipstorage[i].length == shipstorage[i].count) {
-      for(let j = 0; j < shipstorage[i].length ; j++) {
-        let sunken = document.getElementById(`${shipstorage[i].title}${i}${j}`);
-        sunken.classList.add('sunk');
+let shipsunkcount = 0;
+let computershipsunkcount = 0;
+
+
+function isSunk(test) {
+  if(test[0] !== 'c') {
+    for(let i = 0 ; i < shipstorage.length ; i++) {
+      if(shipstorage[i].length == shipstorage[i].count) {
+        shipstorage[i].sunk = true;
+        for(let j = 0; j < shipstorage[i].length ; j++) {
+          let sunken = document.getElementById(`${shipstorage[i].title}${i}${j}`);
+          sunken.classList.add('sunk');
+        }
       }
     }
-  }
-  for (let i = 0; i < computershipstorage.length; i++) {
-    if (computershipstorage[i].length == computershipstorage[i].count) {
-      for (let j = 0; j < computershipstorage[i].length; j++) {
-        let sunken = document.getElementById(`${computershipstorage[i].title}${i}${j}`);
-        sunken.classList.add('sunk');
+    gameover();
+  } else {
+    for (let i = 0; i < computershipstorage.length; i++) {
+      if (computershipstorage[i].length == computershipstorage[i].count) {
+        computershipstorage[i].sunk = true;
+        for (let j = 0; j < computershipstorage[i].length; j++) {
+          let sunken = document.getElementById(`${computershipstorage[i].title}${i}${j}`);
+          sunken.classList.add('sunk');
+        }
       }
     }
+    gameover();
   }
 }
 let ships = 0;
@@ -97,7 +109,6 @@ gameboard(10, 'second')
 
 function ship(board, e){
   let d = parseInt(e)
-  console.log(`${board}${d}`);
   if(shipstorage[ships].max > e ) {
     for(let i = 0 ; i < shipstorage[ships].length; i++) {
       let hovership = document.getElementById(`${board}${d}`)
@@ -117,12 +128,11 @@ function random() {
 function computerboard(c) {
   // need to figure out how to make it work when c < 10
   if(c <= computershipstorage[computerships].max && c > 10) {
-    console.log(taken);
-    console.log(c)
     if (taken.includes(c) == false && taken.includes(c+10) == false && taken.includes(c+20) == false && taken.includes(c+40) == false) {
       let d = c;
       for (let i = 0; i < computershipstorage[computerships].length; i++) {
         let hovership = document.getElementById(`second${d}`);
+        hovership.classList.add("bluetime")
         hovership.setAttribute('id',`${computershipstorage[computerships].title}${computerships}${i}`);
         hovership.addEventListener('click', hit);
         taken.push(d);
@@ -143,3 +153,30 @@ function computerboards() {
   }
 }
 computerboards()
+
+function hitback() {
+  console.log("hi")
+}
+
+function gameover() {
+  let shipcount = 0;
+  for(let i=0; i < shipstorage.length ; i++) {
+
+    if(shipstorage[i].sunk == true) {
+      shipcount++
+    }
+    if(shipcount == 5) {
+      alert('you lose')
+    }
+  }
+  let computershipcount = 0;
+  for (let i = 0; i < computershipstorage.length; i++) {
+    if (computershipstorage[i].sunk == true) {
+      computershipcount++;
+    }
+    if (computershipcount == 5) {
+      alert('you win');
+    }
+  }
+  
+}
