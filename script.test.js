04d625b13a8784777f391c16430028b97432e963
raylu15742cs <raylucs15742@gmail.com>
2022-09-19 09:39:39 -0700

    update testing

--- script.test.js
+++ script.test.js
@@ -1,15 +1,9 @@
-const sum = require('./script').default;
+/**
+ * @jest-environment jsdom
+ */
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(
-    sum([
-      { title: 'Carrier', length: '5' },
-      { title: 'Battleship', length: '4' },
-      { title: 'Cruiser', length: '3' },
-      { title: 'Destroyer', length: '2' },
-      { title: 'Destroyertwo', length: '2' },
-      { title: 'Submarine', length: '1' },
-      { title: 'Submarinetwo', length: '1' },
-    ])
-  ).toBe('Carrier');
+const sum = require('./scripts').default;
+
+test(ships, () => {
+  expect(shipstorage.title).toBe('Carrier');
 });
